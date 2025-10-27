afaf1a32dd2a349feeefd5567e8073bfbcae4cc9
ElKingNoel <lucasruivao@gmail.com>
2025-10-26 22:59:23 -0300

    DoubleVar.lua

---
+++ SimpleScripts/DoubleVar.lua
@@ -0,0 +1,12 @@
+local Var = math.random(1, 5)
+
+print(Var)
+
+function Double()
+	print(Var*2)
+end
+
+Double()
+
+-- This script messes with my favorite school subject, mathematics,
+-- you can change it and put the value you want, it will be sent to the function and the function will process that number and return twice the value
